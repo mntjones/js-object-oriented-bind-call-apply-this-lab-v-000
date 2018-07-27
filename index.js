4259651231585783521c1e8f04aae0f676c49388
Monica Tamaru Jones <mntamaru@gmail.com>
2018-07-27 21:23:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function justInvoke(fn) {
 
 function setThisWithCall(fn, thisValue, arg) {
   const withCall = fn()
-  return withCall
+  return withCall.call(thisValue, arg)
 }
 
 function setThisWithApply(fn, thisValue, args) {
