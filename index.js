2a023550b138c4bf42007a349f604af9e5260b94
Monica Tamaru Jones <mntamaru@gmail.com>
2018-07-27 20:57:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-//Your code here
+function justInvoke(fn) {
+  return fn()
+}
+
+function setThisWithCall(fn, thisValue, arg) {
+  
+}
+
+function setThisWithApply(fn, thisValue, args) {
+  
+}
+
+function returnNewFunctionOf (functionToBeCopied, thisValue) {
+  
+}
